019e8bc3da76f14c8dea77915c5fb3a6fdecf4d5
Peter Asanov <leaxdc@gmail.com>
2015-08-21 22:02:02 +0300

    build fix

--- build_af.sh
+++ build_af.sh
@@ -1,7 +1,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake ../audiofingerprint -DCMAKE_INSTALL_PREFIX=install
+cmake .. -DCMAKE_INSTALL_PREFIX=install
 make -j4 && make install
 cp ../settings_af.json  install/
 cp ../license.lic install/

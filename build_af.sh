2fb35d39b0568744a42d4cc163868e5558ee6aff
Peter Asanov <leaxdc@gmail.com>
2015-08-21 22:01:11 +0300

    gitignore

--- build_af.sh
+++ build_af.sh
@@ -1,7 +1,7 @@
 rm -rf build
 mkdir build
-cp settings_af.json build/
-cp license.lic build/
 cd build
 cmake ../audiofingerprint -DCMAKE_INSTALL_PREFIX=install
 make -j4 && make install
+cp ../settings_af.json  install/
+cp ../license.lic install/

c4a9fe5cbbb0954b8b299ad7e6de3699c1f54dfd
Peter Asanov <leaxdc@gmail.com>
2015-08-22 12:56:54 +0300

    fixing bugs

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cmake .. -DCMAKE_INSTALL_PREFIX=install
 make -j4 && make install
 cp ../settings_af.json  install/
 cp ../license.lic install/
+cp ../teen_spirit_14s.wav install/
 cd install
-./AudioFingerPrint --settings=settings_af.json --input=blah --output=blah
+./AudioFingerPrint --settings=settings_af.json --input=teen_spirit_14s.wav --output=output.json
 cd ../..
